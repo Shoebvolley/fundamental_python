3bf76c428149a47f93dc2818754ec4efcc7d3c47
Shoebvolley <167853161+Shoebvolley@users.noreply.github.com>
2024-05-09 22:51:14 +0530

    Add files via upload

---
+++ Day3 _python  (1).py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+first_name = "shoeb"
+last_name ="syed"
+
+
+# In[3]:
+
+
+full_name =f"{first_name}\t{last_name}"
+print(full_name)
+
+
+# In[4]:
+
+
+print(full_name.title())
+
+
+# In[6]:
+
+
+print("i\nwant\nto\nlearn\nAWS\ncloud\ncomputing\nbut\nthey\nare\nnot\nhiring\nfreshers")
+
+
+# In[7]:
+
+
+print("i\n\twant\n\tto\nlearn\n\tAWS\n\tcloud\ncomputing\nbut\nthey\nare\nnot\nhiring\nfreshers")
+
+
+# In[9]:
+
+
+name ="   python"
+print(name)
+
+
+# In[10]:
+
+
+name.lstrip()
+
+
+# In[11]:
+
+
+name2= "        I want to give my best     "
+print(name2)
+
+
+# In[12]:
+
+
+name2.strip
+
+
+# In[13]:
+
+
+name2.strip()
+
+
+# In[ ]:
+
+
+
+
